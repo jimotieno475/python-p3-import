f3a06a32f8e3ef72acf9e24c51cd355a968e8888
jim <jim.otieno@student.moringaschool.com>
2023-12-04 09:18:48 +0300

    done

--- lib/absolute_package/package1/module1.py
+++ lib/absolute_package/package1/module1.py
@@ -3,5 +3,5 @@ from .module2 import function1
 def function1():
     print('Function 1 in module 1')
 
-import my_package
-my_package.subpackage1.module1.function1()
+# import my_package
+# my_package.subpackage1.module1.function1()
